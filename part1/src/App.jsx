1a1ea1ed7ee0d9405d3995619b384469fbebf9fa
Devansh Kukreja <devansh.mmis@gmail.com>
2024-04-29 22:57:11 +0530

    doneee

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,17 +1,35 @@
 import { useState } from "react";
 import "./App.css";
+import { RecoilRoot, useRecoilValue } from "recoil";
+import { NetworkAtom } from "./atoms";
+import { jobsAtom } from "./atoms";
+import { NotificationAtom } from "./atoms";
+import { MessagingAtom } from "./atoms";
 
 const App = () => {
   return (
-    <>
-    <div className="grid grid-cols-1 md:grid-cols-3">
- <div className="bg-red-500">yoo whats up maan</div>
- <div className="bg-yellow-500">yoo whats up maan</div>
- <div className="bg-green-500">yoo whats up maan</div>
-    </div>
-
-    </>
+    <RecoilRoot>
+      <MainApp />
+    </RecoilRoot>
   );
 };
+
+function MainApp() {
+  const [count, setCount] = useState(0); // Corrected useState syntax
+  const networkNotificationCount = useRecoilValue(NetworkAtom); // Corrected variable name (camelCase)
+  const jobsAtomcount = useRecoilValue(jobsAtom);
+  const NotificationAtomcount = useRecoilValue(NotificationAtom);
+  const MessagingAtomCount = useRecoilValue(MessagingAtom);
+  return (
+    <>
+      <button> home </button>
+      <button> My Network ({networkNotificationCount >= 100 ? "99+" : networkNotificationCount}) </button>
+      <button> Jobs ({jobsAtomcount}) </button>
+      <button> Notifications ({NotificationAtomcount}) </button>
+      <button> Messaging({MessagingAtomCount}) </button>
+      <button> Me </button>
+    </>
+  );
+}
 
 export default App;
---
+++ part1/src/atoms.js
@@ -0,0 +1,18 @@
+import { atom } from 'recoil';
+
+export const NetworkAtom = atom({
+    key: "NetworkAtom",
+    default:104
+});
+export const jobsAtom = atom({
+    key: "jobsAtom",
+    default:0
+});
+export const NotificationAtom = atom({
+    key: "NotificationAtom",
+    default:0
+});
+export const MessagingAtom = atom({
+    key: "MessagingAtom",
+    default:12
+});
